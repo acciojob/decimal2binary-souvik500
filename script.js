0c85ff219ac560bcd2685ffb0dc13396fe4b3d53
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-06-16 10:22:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
+
+
 function threeSum(arr, target) {
 // write your code here
-	arr.sort((a,b)=>a-b);
-	let closetSum = Infinity;
-	for(let i=0;i<arr.length-2;i++){
-		let j =i+1;
-		let k= arr.length-1;
-		while(j<k){
-			const sum =arr[i]+arr[j]+arr[k];
-			if(Math.abs(sum-target)< Math.abs(closetSum-target)){
-				closetSum=sum;
-			}
-			if(sum<target){
-				j++;
-			}
-			else{
-				k--;
-			}
-		}
-	}
-	
-  return closetSum;
+  return dec.tostring(2);
 }
 
 module.exports = threeSum;
